7d7b04121b5ebdf1db20be0e42689868754661ca
Clay Biela <claybiela@gmail.com>
2026-09-14 19:25:12 +0000

    Added minimal gnome

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -26,6 +26,13 @@
   main-user.enable = true;
   main-user.userName = "jim";
   
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ] ;
+  
 
 
 
