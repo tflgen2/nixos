9436024d7d87cb098a1db0dbe57a0f2fe07f1594
Clay Biela <claybiela@gmail.com>
2026-09-14 19:29:50 +0000

    Added firefox

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -100,6 +100,8 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    firefox
+    
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
