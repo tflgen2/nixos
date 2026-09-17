dd2c11098e490e24bb05996a5ac85a581f58cf9f
Clay Biela <claybiela@gmail.com>
2026-09-16 21:30:46 -0400

    configuration.nix all teased out into modules

--- home/default.nix
+++ home/default.nix
@@ -1,20 +1,10 @@
-{ config, pkgs, nixpkgs-unstable, ... }:
+{ pkgs, ... }:
 
-let
-	pkgs-unstable = import nixpkgs-unstable {
-		system = pkgs.stdenv.hostPlatform.system;
-		config.allowUnfree = true;
-};
-in
 {
 	home.stateVersion = "25.11";
-	home.packages = with pkgs; [
-		# add user pkgs here
-		pkgs-unstable.bitwarden-desktop
-		pkgs-unstable.winbox4
-	];
 	imports = [
 		./gnome.nix
 		./zsh.nix
+		./applications.nix
 	];
 }
--- hosts/t440p/configuration.nix
+++ hosts/t440p/configuration.nix
@@ -37,79 +37,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  #networking.networkmanager.enable = true;
   networking.hostName = "t440p";
 
 
-#  # Select internationalisation properties.
-#  i18n.defaultLocale = "en_US.UTF-8";
-#
-#  i18n.extraLocaleSettings = {
-#    LC_ADDRESS = "en_US.UTF-8";
-#    LC_IDENTIFICATION = "en_US.UTF-8";
-#    LC_MEASUREMENT = "en_US.UTF-8";
-#    LC_MONETARY = "en_US.UTF-8";
-#    LC_NAME = "en_US.UTF-8";
-#    LC_NUMERIC = "en_US.UTF-8";
-#    LC_PAPER = "en_US.UTF-8";
-#    LC_TELEPHONE = "en_US.UTF-8";
-#    LC_TIME = "en_US.UTF-8";
-#  };
-
-  #services.xserver.enable = true;
-#
-  #services.displayManager.gdm.enable = true;
-  #services.desktopManager.gnome.enable = true;
-  #services.gnome.gnome-keyring.enable = true;
-#
-  #services.xserver.xkb = {
-    #layout = "us";
-    #variant = "";
-  #};
-#
-  #services.pulseaudio.enable = false;
-  #security.rtkit.enable = true;
-  #services.pipewire = {
-    #enable = true;
-    #alsa.enable = true;
-    #alsa.support32Bit = true;
-    #pulse.enable = true;
-
-  #};
-
-
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #vim 
-    #wget
-    dnsmasq
-    #neofetch
-    #htop
-    #ncdu
-    vlc
-    gnome-tweaks
-    kitty
-    remmina
-    rtorrent
-    qbittorrent
-    #git
-  ];
-
-  programs.winbox.enable = true;
-  #programs.zsh.enable = true;
-  #programs.steam = {
-    #enable = true;
-  #};
-  #programs.gamemode.enable = true;
 
   system.stateVersion = "25.11"; 
 
-  #nixpkgs.config.allowUnfree = true;
-  
-  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
