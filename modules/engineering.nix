6cdc9f73de1f6d4af6aacdd730863656aa2dd5cd
Clay Biela <claybiela@gmail.com>
2026-09-18 09:48:08 -0400

    fixed wireshark installation. Needed to be defined not just in pkgs

--- modules/engineering.nix
+++ modules/engineering.nix
@@ -1,9 +1,20 @@
 { pkgs, ... }:
 
 {
+	users.users.clay = {
+		extraGroups = [
+			"wireshark"
+		];
+	};
+
 	programs.winbox.enable =true;
+	programs.wireshark = {
+		enable =true;
+		package =pkgs.wireshark;
+		dumpcap.enable =true;
+		usbmon.enable =true;
+	};
 	environment.systemPackages = with pkgs; [
 		dnsmasq
-		wireshark
 	];
 }
